495f7081ffb87b40e38f7050d8f594abbe7164aa
agrant4u <alan.johnson4@g.austincc.edu>
2022-02-21 15:52:37 -0600

    Updates
    Camera Trigger!  Updated Snake Hole Behavior that spawns a SnakeDoll.  Spawn Players is part of the multi player stuff with Photon.

---
+++ ClimbOn_Milestone2/ClimbOn_Milestone2/Assets/Scripts/sCameraTrigger.cs
@@ -0,0 +1,108 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+
+
+public enum eCameraState { topRig, middleRig, bottomRig }
+
+public class sCameraTrigger : MonoBehaviour
+{
+
+    public GameObject pFreeLookObject;
+
+    bool isTriggered;
+
+    CinemachineFreeLook freeLookCam;
+
+    CinemachineOrbitalTransposer orbitTransposer;
+
+    CinemachineFreeLook.Orbit[] orbits;
+
+    public eCameraState camState;
+
+    public float transitionTime = 2f;
+
+    float rigAxis;
+
+    float interpolateAmount;
+
+
+    private void Awake()
+    {
+        isTriggered = false;
+        freeLookCam = pFreeLookObject.GetComponent<CinemachineFreeLook>();
+
+        
+
+    }
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+
+        sCharacterController player;
+        player = other.gameObject.GetComponent<sCharacterController>();
+
+        if (player && !isTriggered)
+
+        {
+            Debug.Log("Triggering Camera collier");
+            isTriggered = true;
+            StartCoroutine(CameraSwitch(rigAxis));
+
+        }
+        
+
+
+    }
+
+    void SetYAxis()
+    {
+
+        switch(camState)
+        {
+            case eCameraState.bottomRig:
+                rigAxis = 0;
+
+                break;
+
+            case eCameraState.middleRig:
+                rigAxis = 0.5f;
+
+                break;
+
+            case eCameraState.topRig:
+                rigAxis = 1f;
+
+                break;
+
+        }
+
+    }
+
+
+    IEnumerator CameraSwitch(float _rigAxis)
+    {
+
+        float currentValue = freeLookCam.m_YAxis.Value;
+        float counter = 0;
+
+        //interpolateAmount = (interpolateAmount + Time.deltaTime) % 1f;
+        while(counter<transitionTime)
+        {
+            freeLookCam.m_YAxis.Value = Mathf.Lerp(currentValue, _rigAxis, (counter/transitionTime));
+
+            Debug.Log("Moving Camera from " + currentValue + " to " + _rigAxis);
+
+            counter += Time.deltaTime;
+
+            yield return null;
+        }
+        
+
+        //Destroy(this.gameObject);
+       
+    }
+
+}
