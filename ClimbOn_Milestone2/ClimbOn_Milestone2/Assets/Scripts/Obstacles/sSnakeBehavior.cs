ef63e99eac5f711ada60b143f648b2bad85aba59
agrant4u <alan.johnson4@g.austincc.edu>
2022-03-22 13:23:06 -0500

    updates
    Nothing big with this one.

--- ClimbOn_Milestone2/ClimbOn_Milestone2/Assets/Scripts/Obstacles/sSnakeBehavior.cs
+++ ClimbOn_Milestone2/ClimbOn_Milestone2/Assets/Scripts/Obstacles/sSnakeBehavior.cs
@@ -2,17 +2,37 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody))]
+
 public class sSnakeBehavior : MonoBehaviour
 {
+    Rigidbody rb;
+
+    Vector3 movementDir;
+    
+    float movementSpeed = 0.01f;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
+        movementDir = new Vector3(movementSpeed, 0, 0);
+
+        rb = GetComponent<Rigidbody>();
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        movementDir = new Vector3(movementSpeed, 0, 0);
+
+        rb.MovePosition(movementDir);
+
+        movementSpeed+=Time.deltaTime * 4;
+
+        //rb.position += movementDir;
+
+       // rb.velocity += movementDir;
     }
 }
